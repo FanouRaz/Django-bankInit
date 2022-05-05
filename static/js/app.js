595e1317a9fa53daf5fb3bbbf0311c2152bdd941
Fanou <fanourazak@gmail.com>
2022-05-05 22:13:13 +0300

    Implementation et ajout du breadcrumb sur chaque page

--- static/js/app.js
+++ static/js/app.js
@@ -18,3 +18,34 @@ for(let bouton of boutons){
         }
     });
 }
+
+
+/*--------------------Breadcrumb----------------------*/
+//Implémentation du breadcrumb
+let url = window.location.pathname
+let links = {
+    '/dashboard':'Dashboard',
+    '/liste':'Liste de prêts',
+    '/formulaire':'Enregistrement d\'un nouveau prêt',
+    '/calendrier':'Calendrier de remboursement'
+};
+let breadcrumb = document.querySelector('.breadcrumb');
+let breadcrumbLink = breadcrumb.childNodes;
+console.log(breadcrumbLink);
+for(let link in links){
+    if(url == link){
+        let separateur = document.createTextNode('/');
+        let currentPage = document.createElement('a');
+        currentPage.textContent = links[link];
+        currentPage.classList.add('currentPage');
+        console.log(currentPage.textContent);
+        if(breadcrumbLink.length == 3){
+            breadcrumbLink[1].classList.remove('currentPage');
+            breadcrumb.appendChild(separateur);
+            breadcrumb.appendChild(currentPage);
+        }
+        else{
+            breadcrumbLink[4] = currentPage;
+        }
+    }
+}
